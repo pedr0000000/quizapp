f84641d3c1df4835aab3c9285e82431c51d87101
pedr0000000 Pedro Henrique <pedro.santos0908@eaportal.org>
2024-05-21 17:07:09 +0000

    alteração dinâminca do assunto da página

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -8,4 +8,21 @@ botaoTema.addEventListener("click", () => {
     trocarTema (body, botaoTema)
 })
 
-verificarTema (body, botaoTema)
+verificarTema (body, botaoTema) 
+
+const assunto = localStorage.getItem("assunto")
+
+
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntosTitulo = document.querySelector(".assunto h1")  
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLocaleLowerCase()}.svg`)
+    assuntosTitulo.innerText = assunto
+    iconeImg.setAttribute("alt", `icone de ${assunto}`)
+}
+
+alterarAssunto()
